acb5a86541b8c3f0bf33f4578ab387a0368a0ea2
Louis-Mont <Louis-Mont@users.noreply.github.com>
2021-03-10 12:26:45 +0100

    Understood how add and edit were working

--- Api/Api.py
+++ Api/Api.py
@@ -1,15 +1,22 @@
 from datetime import datetime
-
+from tkinter import Tk
+from gw_logging.Log import Log
 from Core.Core import Core
 from db_interface.DatabaseODBC import DatabaseODBC
 from prestapyt import PrestaShopWebServiceDict
-
 from utils import l_to_str, rev_col
 from yesno import yesno
 
 
 class Api(Core):
     def __init__(self, frame, log_interface, dsn, ip, key):
+        """
+        :type frame: Tk
+        :type log_interface: Log
+        :type dsn: str
+        :type ip: str
+        :type key: str
+        """
         super().__init__(frame, log_interface)
         self.dsn = dsn
         self.ip = ip
@@ -46,71 +53,94 @@ class Api(Core):
                 (gdr_prod['IDSortie'], ([0, 1], True, "Ce produit ne peut pas être mis en vente"))
             ]
             if self.requirements(reqs):
-                # Ajout ou non de catégorie
-                cat_exists = False
-                id_cat = 0
-                for id_cat in self.get_indexes(self.api.get('categories')['categories']['category']):
-                    # debug = self.api.get('categories', id_cat)
-                    if self.api.get('categories', id_cat)['category']['name']['language'][0]['attrs'] == cat_name:
-                        cat_exists = True
-                        break
-                if cat_exists:
-                    self.i_log.add("Catégorie trouvée")
-                else:
-                    self.i_log.add(f"Ajout {cat_name}")
-                    self.add_cat(cat_name)
-                    id_cat += 1
-
-                # Ajout produit
-                date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                prod_schema = self.api.get('products', options={'schema': 'blank'})['product']
-                prod_dict = {
-                    'id_category_default': f"{id_cat}",
-                    'id_shop_default': f"{1}",
-                    'reference': f"{id_product}",
-                    'quantity': f"{gdr_prod['Nombre']}",
-                    'width': f"{gdr_prod['Largeur']}",
-                    'height': f"{gdr_prod['Hauteur']}",
-                    'depth': f"{gdr_prod['Profondeur']}",
-                    'weight': f"{gdr_prod['Poids']}",
-                    'state': f"{1}",
-                    'minimal_quantity': f"{1}",
-                    'price': f"{float(gdr_prod['PrixUnitaire'])}",
-                    'wholesale_price': f"{int(gdr_prod['PrixUnitaire']) * int(gdr_prod['Nombre'])}",
-                    'active': f"{1}",
-                    'redirect_type': f"'301-product'",
-                    'available_for_order': f"{1}",
-                    'show_condition': f"{1}",
-                    'show_price': f"{1}",
-                    'condition': f"'refurbished'",
-                    'date_add': f"'{date}'",
-                    'date_upd': f"'{date}'"
-                }
-                if float(prod_dict['price']) < 1:
-                    self.i_log.add("Le prix de ce produit était à 0, il a été mis à 1")
-                    prod_dict['price'] = 1.0
-                prod_schema = {**prod_schema, **prod_dict}
-                link_rewrite = cat_name.lower().encode("ascii", "ignore").decode("utf-8")
-                self.set_lang(prod_schema, 'link_rewrite', link_rewrite)
-                self.set_lang(prod_schema, 'name', title)
-
                 prod_exists = False
                 id_prod = 0
-                for id_prod in self.get_indexes(self.api.get('products')['products']['product']):
+                for id_prod in self.get_indexes(('product', 'products')):
                     if self.api.get('products', id_prod)['product']['reference'] == id_product:
                         prod_exists = True
                 if prod_exists:
-                    prod_schema['id'] = f"{id_prod}"
                     self.i_log.add(f"La référence {id_product} existe déjà dans la base de données")
                     yesno(self.frame, "Duplicata",
                           "La référence existe déjà dans la base de données, voulez-vous la mettre à jour?",
-                          lambda: self.api.edit('products', prod_schema))
+                          lambda: self.do_prod(id_product, title, cat_name, gdr_prod, id_prod, True))
                 else:
-                    self.api.add('products', prod_schema)
+                    self.do_prod(id_product, title, cat_name, gdr_prod)
         else:
             self.i_log.add(f"ID {id_product} incorrecte")
 
-    def add_cat(self, cat_name):
+    def do_prod(self, id_product, title, cat_name, gdr_prod, edit=False):
+        # Ajout ou non de catégorie
+        cat_exists = self.x_exists(('category', 'categories'), 'name', cat_name)
+        if cat_exists[0]:
+            self.i_log.add("Catégorie trouvée")
+        else:
+            self.i_log.add(f"Ajout {cat_name}")
+            self.add_cat(cat_name)
+            id_cat = cat_exists[1] + 1
+
+        # Ajout produit
+        date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        prod_schema = self.api.get('products', options={'schema': 'blank'})['product']
+        prod_dict = {
+            'id_category_default': f"{id_cat}",
+            'id_shop_default': f"{1}",
+            'reference': f"{id_product}",
+            'width': f"{gdr_prod['Largeur']}",
+            'height': f"{gdr_prod['Hauteur']}",
+            'depth': f"{gdr_prod['Profondeur']}",
+            'weight': f"{gdr_prod['Poids']}",
+            'state': f"{1}",
+            'minimal_quantity': f"{1}",
+            'price': float(gdr_prod['PrixUnitaire']),
+            'wholesale_price': f"{int(gdr_prod['PrixUnitaire']) * int(gdr_prod['Nombre'])}",
+            'active': f"{1}",
+            'redirect_type': f"'301-product'",
+            'available_for_order': f"{1}",
+            'show_condition': f"{1}",
+            'show_price': f"{1}",
+            'condition': f"'refurbished'",
+            'date_add': f"'{date}'",
+            'date_upd': f"'{date}'"
+        }
+        if float(prod_dict['price']) < 1:
+            self.i_log.add("Le prix de ce produit était à 0, il a été mis à 1")
+            prod_dict['price'] = 1.0
+        prod_schema = {**prod_schema, **prod_dict}
+        link_rewrite = cat_name.lower().encode("ascii", "ignore").decode("utf-8")
+        self.set_lang(prod_schema, 'link_rewrite', link_rewrite)
+        self.set_lang(prod_schema, 'name', title)
+        ps_id_prod = len(self.get_indexes(('product', 'products'))) + 1
+        if edit:
+            prod_dict['id'] = f"{ps_id_prod}"
+            self.api.edit('products', {'product': prod_dict})
+        else:
+            self.api.add('products', {'product': prod_schema})
+
+        combination_schema = self.api.get('combination', options={'schema': 'blank'})['combination']
+        combination_dict = {
+            'id_product': f"{ps_id_prod}"
+        }
+        combination_schema = {**combination_schema, **combination_dict}
+        for k, v in combination_schema.items():
+            try:
+                combination_schema[k] = prod_schema[k]
+            except KeyError:
+                pass
+        self.api.add('combination', {'combination': combination_schema})
+
+        sa_schema = self.api.get('stock_available', options={'schema': 'blank'})['stock_available']
+        sa_dict = {
+            'id_product': f"{ps_id_prod}",
+            'id_product_attribute': f"{len(self.get_indexes(('combinations', ('combination')))) + 1}",
+            'id_shop': f"{1}",
+            'quantity': f"{gdr_prod['Nombre']}",
+            'depends_on_stock': f"{1}",
+            'out_of_stock': f"{2}"
+        }
+        sa_schema = {**sa_schema, **sa_dict}
+        self.api.add('stock_available', {'stock_available': sa_schema})
+
+    def add_cat(self, cat):
         cat_schema = self.api.get('categories', options={'schema': 'blank'})['category']
         date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         cat_dict = {
@@ -122,15 +152,15 @@ class Api(Core):
         }
         # https://stackoverflow.com/questions/38987/how-do-i-merge-two-dictionaries-in-a-single-expression-taking-union-of-dictiona/228366#228366
         cat_schema = {**cat_schema, **cat_dict}
-        self.set_lang(cat_schema, 'name', cat_name)
-        link_rewrite = cat_name.lower().encode("ascii", "ignore").decode("utf-8")
+        self.set_lang(cat_schema, 'name', cat)
+        link_rewrite = cat.lower().encode("ascii", "ignore").decode("utf-8")
         self.set_lang(cat_schema, 'link_rewrite', link_rewrite)
 
     def sync_ventes(self):
         self._connect()
         gdr_cur = self.gdr_db.DB.cursor()
         self.i_log.add("Synchronisation des ventes")
-        for id_prod in self.get_indexes(self.api.get('products')['products']['product']):
+        for id_prod in self.get_indexes(('product', 'products')):
             nvc = rev_col(self.ventes_cols)
             reference = self.api.get('products', id_prod)['product']['reference']
             gdr_cur.execute(
@@ -142,7 +172,7 @@ class Api(Core):
                     if s1[nvc['IDProduit']] == s2[nvc['IDProduit']]:
                         ex_sum += s2[nvc['Montant']]
                 if ex_sum >= 0:
-                    if self.x_exists(('product', 'products'), 'reference', str(id_prod)):
+                    if self.x_exists(('product', 'products'), 'reference', str(id_prod))[0]:
                         prod_schema = self.api.get('products', options={'schema': 'blank'})['product']
                         prod = {
                             'on_sale': f"{0}",
@@ -156,13 +186,17 @@ class Api(Core):
 
     def set_lang(self, schema, key, name):
         # e.g. : name : {'language': [{'attrs': {'id': '1'}, 'value': ''}, {'attrs': {'id': '2'}, 'value': ''}]}
-        for langs in schema[key]['language']:
-            langs['value'] = name
-        return schema
+        try:
+            for langs in schema[key]['language']:
+                langs['value'] = name
+            return schema
+        except KeyError:
+            schema[key] = {'language': [{'attrs': {'id': '1'}, 'value': ''}, {'attrs': {'id': '2'}, 'value': ''}]}
+            return self.set_lang(schema, key, name)
 
     def reset_db(self):
-        self.api.delete('product', self.get_indexes(self.api.get('products')['products']['product']))
-        self.api.delete('categories', self.get_indexes(self.api.get('categories')['categories']['category']))
+        self.api.delete('product', self.get_indexes(('product', 'products')))
+        self.api.delete('categories', self.get_indexes(('category', 'categories')))
 
     def x_exists(self, head_name, x, name):
         """
@@ -172,12 +206,19 @@ class Api(Core):
         :param x: The column you're looking into
         :type x: str
         :type name: str
-        :rtype: bool
+        :return: [0] : exists?, [1]: where in the list
+        :rtype: tuple
         """
-        for id_cat in self.get_indexes(self.api.get(head_name[1])[head_name[1]][head_name[0]]):
-            if self.api.get(head_name[1], id_cat)[x]['language'][0]['value'] == name:
-                return True
-        return False
+        for id in self.get_indexes(head_name):
+            if self.api.get(head_name[1], id)[head_name[0]][x]['language'][0]['value'] == name:
+                return True, id
+        return False, id
 
-    def get_indexes(self, obj_list):
-        return [int(attr['attrs']['id']) for attr in obj_list]
+    def get_indexes(self, head_name):
+        """
+        Get all indexes for the head_name
+        :param head_name: singular[0], plural[1]
+        :type head_name: tuple
+        :rtype: list
+        """
+        return [int(attr['attrs']['id']) for attr in self.api.get(head_name[1])[head_name[1]][head_name[0]]]
