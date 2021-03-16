24f0bda76d6bb78f0bf3e4847f5b45e5c2ce2082
Louis-Mont <Louis-Mont@users.noreply.github.com>
2021-03-16 10:39:12 +0100

    Fixed typo

--- Api/DebugApi.py
+++ Api/DebugApi.py
@@ -1,7 +1,7 @@
 class DebugApi:
     def __init__(self, force_reqs=False, force_yes=False):
         """
-        :param force_reqs: Forces the reqs even if they are wrong
+        :param force_reqs: Forces the requirements even if they are wrong
         :param force_yes: Forces the yes on the edit popup
         """
         self.force_reqs = force_reqs
